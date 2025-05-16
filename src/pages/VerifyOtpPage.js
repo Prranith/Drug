613ca2fba4e5722cf6dd80a31ba2bdc16690a5e1
Prranith Swargam <160862370+Prranith@users.noreply.github.com>
2025-05-17 01:09:37 +0530

    Update VerifyOtpPage.js

--- src/pages/VerifyOtpPage.js
+++ src/pages/VerifyOtpPage.js
@@ -23,7 +23,7 @@ const VerifyOtpPage = () => {
     if (otp) {
       try {
         const response = await axios.post(
-          'https://rehab-df-described-jason.trycloudflare.com/api/auth/verify-otp',
+          'https://5e69-2401-4900-4fe7-3cc7-1594-65c6-8521-d5d4.ngrok-free.app/api/auth/verify-otp',
           { email, otp, username, password },
           { withCredentials: true }
         );
